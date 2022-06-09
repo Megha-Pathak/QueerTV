0722f688b83420518ef9e98d5f6896160b69a9e0
megha-pathak <meghapathak2013@gmail.com>
2022-06-09 17:05:48 +0530

    Window resize hook

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,9 +1,1 @@
 export { HomePage } from "./HomePage/HomePage";
-export { SignInPage } from "./SignInPage/SignInPage";
-export { SignUpPage } from "./SignUpPage/SignUpPage";
-export { ExplorePage } from "./ExplorePage/ExplorePage";
-export { VideoPage } from "./VideoPage/VideoPage";
-export { LikesPage } from "./LikesPage/LikesPage";
-export { WatchLaterPage } from "./WatchLaterPage/WatchLaterPage";
-export { HistoryPage } from "./HistoryPage/HistoryPage";
-export { EmptyStatePage } from "./EmptyStatePage/EmptyStatePage";
