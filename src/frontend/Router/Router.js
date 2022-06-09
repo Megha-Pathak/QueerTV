0c4ec579611eef7f4d1dd939578e80864f9d40de
megha-pathak <meghapathak2013@gmail.com>
2022-06-09 22:31:52 +0530

    Add Explore List

--- src/frontend/Router/Router.js
+++ src/frontend/Router/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { HomePage, SignInPage, SignUpPage } from "./../pages";
+import { ExplorePage, HomePage, SignInPage, SignUpPage } from "./../pages";
 import "./Router.css";
 
 export const Router = () => {
@@ -10,6 +10,7 @@ export const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/explore" element={<ExplorePage />} />
       </Routes>
     </div>
   );
--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -4,3 +4,5 @@ export { Search } from "./Search/Search";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { MainSidebar } from "./MainSidebar/MainSidebar";
 export { ToggledSidebar } from "./ToggledSidebar/ToggledSidebar";
+export { Categories } from "./Categories/Categories";
+export { ExploreVideoCard } from "./ExploreVideoCard/ExploreVideoCard";
