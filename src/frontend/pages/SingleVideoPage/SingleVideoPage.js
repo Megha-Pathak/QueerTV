750430a0265d6277c9a2a9a80d234fc1426f5b9c
megha-pathak <meghapathak2013@gmail.com>
2022-06-09 22:57:10 +0530

    Add Single Video Page

---
+++ src/frontend/pages/SingleVideoPage/SingleVideoPage.js
@@ -0,0 +1,220 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
+import Moment from "react-moment";
+import {
+  useAuth,
+  useHistory,
+  useLikes,
+  useSidebar,
+  useVideos,
+  useWatchLater,
+} from "../../contexts";
+import {
+  addHistoryService,
+  addLikeService,
+  addWatchLaterService,
+  removeHistoryService,
+  removeLikeService,
+  removeWatchLaterService,
+} from "../../services";
+import {
+  SET_HISTORY,
+  SET_LIKES,
+  SET_WATCH_LATER,
+} from "../../constants/queer-constants";
+import "./SingleVideoPage.css";
+
+export const SingleVideoPage = () => {
+  const singleVideoPageRef = useRef();
+  const [singleVideoPageWidth, setSingleVideoPageWidth] = useState();
+
+  const { videoId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { auth } = useAuth();
+  const { showSidebar } = useSidebar();
+  const { videos } = useVideos();
+  const { likes, dispatchLikes } = useLikes();
+  const { watchLater, dispatchWatchLater } = useWatchLater();
+  const { history, dispatchHistory } = useHistory();
+
+  const currentVideo = videos.find((video) => video._id === videoId);
+
+  const getSingleVideoPageSize = () => {
+    const newWidth = singleVideoPageRef.current.clientWidth;
+    setSingleVideoPageWidth(newWidth);
+  };
+
+  useEffect(() => {
+    getSingleVideoPageSize();
+  }, [videos, showSidebar]);
+
+  useEffect(() => {
+    window.addEventListener("resize", getSingleVideoPageSize);
+  }, []);
+
+  const addLikeHandler = async () => {
+    const addLikeResponse = await addLikeService(auth.token, currentVideo);
+    if (addLikeResponse !== undefined) {
+      dispatchLikes({ type: SET_LIKES, payload: addLikeResponse });
+    }
+  };
+
+  const removeLikeHandler = async () => {
+    const removeLikeResponse = await removeLikeService(auth.token, videoId);
+    if (removeLikeResponse !== undefined) {
+      dispatchLikes({ type: SET_LIKES, payload: removeLikeResponse });
+    }
+  };
+
+  const addWatchLaterHandler = async () => {
+    const addWatchLaterResponse = await addWatchLaterService(
+      auth.token,
+      currentVideo
+    );
+    if (addWatchLaterResponse !== undefined) {
+      dispatchWatchLater({
+        type: SET_WATCH_LATER,
+        payload: addWatchLaterResponse,
+      });
+    }
+  };
+
+  const removeWatchLaterHandler = async () => {
+    const removeWatchLaterResponse = await removeWatchLaterService(
+      auth.token,
+      videoId
+    );
+    if (removeWatchLaterResponse !== undefined) {
+      dispatchWatchLater({
+        type: SET_WATCH_LATER,
+        payload: removeWatchLaterResponse,
+      });
+    }
+  };
+
+  const removeHistoryHandler = async () => {
+    const removeHistoryResponse = await removeHistoryService(
+      auth.token,
+      videoId
+    );
+    if (removeHistoryResponse !== undefined) {
+      dispatchHistory({ type: SET_HISTORY, payload: removeHistoryResponse });
+    }
+  };
+
+  const addHistoryHandler = async () => {
+    if (history.find((historyVideo) => historyVideo._id === videoId)) {
+      removeHistoryHandler();
+    }
+    const addHistoryResponse = await addHistoryService(
+      auth.token,
+      currentVideo
+    );
+    if (addHistoryResponse !== undefined) {
+      dispatchHistory({ type: SET_HISTORY, payload: addHistoryResponse });
+    }
+  };
+
+  return (
+    <div
+      ref={singleVideoPageRef}
+      className="video-page"
+      style={{
+        flexDirection: singleVideoPageWidth < 1165 ? "column" : "row",
+      }}
+    >
+      <div
+        className="video-section"
+        style={{ alignSelf: singleVideoPageWidth < 1165 && "stretch" }}
+      >
+        <div className="video-player">
+          <ReactPlayer
+            width="100%"
+            height="100%"
+            url={`http://www.youtube.com/watch?v=${videoId}`}
+            controls
+            playing
+            onStart={() => auth.status && addHistoryHandler()}
+          />
+        </div>
+        <div className="video-section-primary">
+          <h1 className="video-title">{currentVideo?.title}</h1>
+          <div className="video-info">
+            <div className="video-info-primary">
+              <span>{currentVideo?.views} views</span>
+              <span className="material-icons vertical-video-card-secondary-separator">
+                fiber_manual_record
+              </span>
+              <Moment fromNow>{currentVideo?.uploadedAt}</Moment>
+            </div>
+            <div className="video-info-secondary">
+              {likes?.find((like) => like._id === videoId) ? (
+                <button
+                  className="video-action-button"
+                  onClick={removeLikeHandler}
+                >
+                  <span
+                    className="material-icons video-icon"
+                    title="I like this"
+                  >
+                    thumb_up
+                  </span>
+                </button>
+              ) : (
+                <button
+                  className="video-action-button"
+                  onClick={() =>
+                    auth.status
+                      ? addLikeHandler()
+                      : navigate("/signin", { state: { from: location } })
+                  }
+                >
+                  <span
+                    className="material-icons-outlined video-icon"
+                    title="I like this"
+                  >
+                    thumb_up
+                  </span>
+                </button>
+              )}
+              {watchLater?.find(
+                (watchLaterVideo) => watchLaterVideo._id === videoId
+              ) ? (
+                <button
+                  className="video-action-button"
+                  onClick={removeWatchLaterHandler}
+                >
+                  <span
+                    className="material-icons video-icon"
+                    title="I will watch this later"
+                  >
+                    watch_later
+                  </span>
+                </button>
+              ) : (
+                <button
+                  className="video-action-button"
+                  onClick={() =>
+                    auth.status
+                      ? addWatchLaterHandler()
+                      : navigate("/signin", { state: { from: location } })
+                  }
+                >
+                  <span
+                    className="material-icons-outlined video-icon"
+                    title="I will watch this later"
+                  >
+                    watch_later
+                  </span>
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -2,3 +2,4 @@ export { SignInPage } from "./AuthPages/SignInPage";
 export { SignUpPage } from "./AuthPages/SignUpPage";
 export { HomePage } from "./HomePage/HomePage";
 export { ExplorePage } from "./ExplorePage/ExplorePage";
+export { SingleVideoPage } from "./SingleVideoPage/SingleVideoPage";
