6f19f167fcc64f8175d7209d5fd686a8f612a1ba
megha-pathak <meghapathak2013@gmail.com>
2022-06-09 16:57:33 +0530

    Create index.js

---
+++ src/frontend/pages/index.js
@@ -0,0 +1,9 @@
+export { HomePage } from "./HomePage/HomePage";
+export { SignInPage } from "./SignInPage/SignInPage";
+export { SignUpPage } from "./SignUpPage/SignUpPage";
+export { ExplorePage } from "./ExplorePage/ExplorePage";
+export { VideoPage } from "./VideoPage/VideoPage";
+export { LikesPage } from "./LikesPage/LikesPage";
+export { WatchLaterPage } from "./WatchLaterPage/WatchLaterPage";
+export { HistoryPage } from "./HistoryPage/HistoryPage";
+export { EmptyStatePage } from "./EmptyStatePage/EmptyStatePage";
