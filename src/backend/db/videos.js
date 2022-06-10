c4a2f5c67bc5e153ccecfadd80bd16012bd4037c
megha-pathak <meghapathak2013@gmail.com>
2022-06-10 08:24:33 +0530

    Add Videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,90 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "XxrA4hx6EQQ",
+    title:
+      "QueerTV | Gender | LGBTQIA+ lets learn the gender spectrum | Maya's Amma - Malayalam",
+    creator: "Maya's Amma - Malayalam",
+    views: 43484,
+    uploadedAt: new Date("Mar 5, 2021"),
+    categoryName: "Gender",
+    description: `
+    It’s important as parents we create a gender inclusive space at home where everyone is accepted and loved just the way they are.
+<br/>
+    Of course understanding the gender spectrum is important but even if you don’t understand you can always say love is love. That’s enough for children to figure out that there is nothing wrong with them if they fall anywhere in the spectrum and are not binary.
+    <br/>
+    Remember that gender is in the brain and not in the body. When parents accept their children and their sexuality openly we can prevent a lot of mental health issues, children running away from their families, even suicides.
+    <br/>
+    We might not see many lesbian or gay couples around us but that doesn’t mean they are not there. They might be closeted, living their lives in heterosexual marriages not being able to open up and be who they truly are.
+    <br/>
+    I wish for a day when couples are called just couples and not homosexual couples or trans couple. After all we don’t call heterosexual couples with this label right?
+    <br/>
+    But until that level of openness and acceptance comes, it helps to have these labels and awareness days and visibility weeks. Until then, let’s Call it what it is, name it, own it and let people live their own truths.
+    `,
+  },
+
+  {
+    _id: "i6A7UAH5fcY",
+    title: "QueerTV | Sexuality | What Does It Mean to Be Asexual? | Psych2Go",
+    creator: "Psych2Go",
+    views: 1203027,
+    uploadedAt: new Date("Nov 17, 2020"),
+    categoryName: "Sexuality",
+    description: `
+    Have you ever wondered whether or not you’re asexual? 
+    <br/>
+    People who identify as asexual – or ace – are not typically sexually attracted to anyone. Asexuality is an extremely misunderstood sexual identity, so we would like to raise more awareness about this. 
+    <br/>
+    Confused about what it means to be asexual? This video is for you! `,
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "ivfJJh9y1UI",
+    title:
+      "This Is What LGBT Life Is Like Around the World | Jenni Chang and Lisa Dazols | TED Talks",
+    creator: "TED",
+    views: 1043572,
+    uploadedAt: new Date("Dec 4, 2015"),
+    categoryName: "Sexuality",
+    description: `
+    As a gay couple in San Francisco, Jenni Chang and Lisa Dazols had a relatively easy time living the way they wanted. But outside the bubble of the Bay Area, what was life like for people still lacking basic rights?
+    <br/>
+    They set off on a world tour in search of "Supergays," LGBT people who were doing something extraordinary in the world. In 15 countries across Africa, Asia and South America — from India, recently home to the world's first openly gay prince, to Argentina, the first country in Latin America to grant marriage equality — they found the inspiring stories and the courageous, resilient and proud Supergays they had been looking for.
+`,
+  },
+  {
+    _id: "W5W_eWJE5sQ",
+    title: "Elana Rubin Explains What Pansexual Means | InQueery | them.",
+    creator: "them",
+    views: 499485,
+    uploadedAt: new Date("Nov 20, 2018"),
+    categoryName: "Sexuality",
+    description: `
+    How much do you really know about the history of the word "pansexual"? Elana Rubin explains on this episode of InQueery.
+`,
+  },
+  {
+    _id: "CTTYwhZ7948",
+    title: "We're Raising Our Kids With No Gender | MY EXTRAORDINARY FAMILY",
+    creator: "truly",
+    views: 1730834,
+    uploadedAt: new Date("Dec 24, 2020"),
+    categoryName: "Gender",
+    description: `
+    A THROUPLE are bringing up their two-year-old baby as 'theyby', a term that refers to gender neutral parenting where the baby isn't outwardly identified by its parents as either a boy or a girl. Baby Sparrow follows in the footsetps of their 10-year-old sibling, Hazel, who was initially brought up as a girl but came out themselves as non-binary aged just four. Hazel and Sparrow are looked after by three parent figures: Ari, Gwendolyn and Brynnifer, who themselves are either non-binary and/or transgender. While the family, from Orlando, Florida, lives within a supportive community, online critics have argued it’s no wonder the children are ‘confused’ about their gender. 
+`,
+  },
+
+  {
+    _id: "YBW7p8_fT2I",
+    title: "4 Demisexual People Explain What Demisexuality Means To Them    ",
+    creator: "Tinder",
+    views: 743027,
+    uploadedAt: new Date("May 22, 2019"),
+    categoryName: "Sexuality",
+    description: `
+    Here at Tinder, we believe that everyone deserves the freedom to live how they want to live and love how they want to love. Inclusivity is in our DNA. 
+<br/>
+    From giving you the option to identify how you feel most authentic through More Genders to getting interracial couple emojis passed through Unicode, fighting for equality and representation is at the heart of what we do. And that’s why we’re particularly excited to bring you a new series that celebrates and turns the mic over to those whose identities don’t always receive the visibility and acceptance they deserve. 
+    `,
   },
 ];
